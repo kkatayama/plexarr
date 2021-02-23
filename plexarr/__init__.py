38464948f433085811300ce259484e1cf18a00de
kkatayama <katayama@udel.edu>
2021-02-23 10:37:41 -0500

    remove config from init()

--- plexarr/__init__.py
+++ plexarr/__init__.py
@@ -1,7 +1,7 @@
-import config
+#import config
 #from .requests_api import RequestsAPI
 from .radarr_api import RadarrAPI
 
 #__all__ = ['config', 'RadarrAPI', 'RequestsAPI']
-__all__ = ['config', 'RadarrAPI']
+__all__ = ['RadarrAPI']
 

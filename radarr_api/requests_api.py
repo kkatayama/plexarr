304e24613e4480daef067bcc11dec2ef8d20e0dd
kkatayama <katayama@udel.edu>
2021-02-22 19:00:48 -0500

    git init

---
+++ radarr_api/requests_api.py
@@ -0,0 +1,33 @@
+from urllib.parse import urljoin
+import requests
+
+
+class RequestsAPI:
+    """Wrapper for requests()
+    """
+    def __init__(self, api_url: str, api_key: str):
+        """Constructor requires API-URL and API-KEY
+        Args:
+            api_url (str): API url for sonarr or radarr.
+            api_key (str): API key for sonarr or radarr.
+        """
+
+        self.api_url = api_url.strip('/') + '/'
+        self.api_key = api_key
+        self.session = requests.Session()
+        self.auth = None
+        self.headers = {
+            'X-Api-Key': api_key,
+            'Content-Type': 'application/json'
+        }
+
+    def get(self, path, data):
+        """Wrapper on session.get()
+        Args:
+            path: The endpoint for API
+            data: Parameters to pass in dict() format
+        """
+
+        url = urljoin(self.api_url, path.strip('/'))
+        res = self.session.get(url=url, headers=self.headers, params=data)
+        return res.json()

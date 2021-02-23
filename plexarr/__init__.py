e3759c24aec49a320ade4886e3cf498f88601fff
kkatayama <katayama@udel.edu>
2021-02-23 10:32:59 -0500

    remove config and RequestsAPI form init()

--- plexarr/__init__.py
+++ plexarr/__init__.py
@@ -1,5 +1,7 @@
-import config
-from .requests_api import RequestsAPI
+#import config
+#from .requests_api import RequestsAPI
 from .radarr_api import RadarrAPI
 
-__all__ = ['config', 'RadarrAPI', 'RequestsAPI']
+#__all__ = ['config', 'RadarrAPI', 'RequestsAPI']
+__all__ = ['RadarrAPI']
+

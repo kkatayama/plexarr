cd78885688c5d4002393541f2db0e2adaa45c0b9
kkatayama <katayama@udel.edu>
2021-02-22 19:19:14 -0500

    git init

---
+++ radarr_api/__init__.py
@@ -0,0 +1,5 @@
+import .config as config
+from .requests_api import RequestsAPI
+from .radarr_api import Radarr
+
+__all__ = ['config', 'Radarr', 'RequestsAPI']
---
+++ radarr_api/radarr_api.py
@@ -0,0 +1,22 @@
+import config
+from requests_api import RequestsAPI
+
+
+class Radarr(RequestsAPI):
+    def __init__(self):
+        """Constructor requires API-URL and API-KEY
+
+        From config:
+            api_url (str): API url for sonarr or radarr.
+            api_key (str): API key for sonarr or radarr.
+        """
+
+        self.api_url = config.radarr.get('api_url')
+        self.api_key = config.radarr.get('api_key')
+        super().__init__(api_url=self.api_url, api_key=self.api_key)
+
+    def getMovies(self):
+        path = '/movie'
+        data = {}
+        res = self.get(path=path, data=data)
+        return res

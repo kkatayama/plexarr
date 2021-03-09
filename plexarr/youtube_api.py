887b623efdcc4208851452aad4e8d8eaa0b09317
kkatayama <katayama@udel.edu>
2021-03-09 15:03:15 -0500

    downloadMovie(): added "FINISHED..."

--- plexarr/youtube_api.py
+++ plexarr/youtube_api.py
@@ -64,6 +64,7 @@ class YouTubeAPI(object):
         }
         with youtube_dl.YoutubeDL(ytdl_opts) as ytdl:
             ytdl.download([video_url])
+        print('FINISHED...')
 
 
     def getInfo(self, path='', video_url=''):

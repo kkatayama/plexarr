cf196578cb60c11c008fb56fbb3419b15101803e
kkatayama <katayama@udel.edu>
2021-03-09 14:40:05 -0500

    added downloadMovie()

--- plexarr/youtube_api.py
+++ plexarr/youtube_api.py
@@ -2,14 +2,15 @@ from configparser import ConfigParser
 import youtube_dl
 import os
 
+
 class MyLogger(object):
     def debug(self, msg):
-        #print(msg)
-        pass
+        print(msg)
+        # pass
 
     def warning(self, msg):
-        #print(msg)
-        pass
+        print(msg)
+        # pass
 
     def error(self, msg):
         print(msg)
@@ -18,13 +19,6 @@ class MyLogger(object):
 def my_hook(d):
     if d['status'] == 'finished':
         print('Done downloading, now converting ...')
-
-def downloadMovie(path, video_url):
-    ydl_opts = {
-        'cookiefile': '/Users/katayama/cookies.txt',
-        'logger': MyLogger(),
-        'progress_hooks': [my_hook]
-    }
 
 
 class YouTubeAPI(object):
@@ -39,7 +33,37 @@ class YouTubeAPI(object):
         config = ConfigParser()
         config.read(os.path.join(os.path.expanduser('~'), '.config', 'plexarr.ini'))
 
+        self.path = config['youtube'].get('path')
         self.cookies = config['youtube'].get('cookies')
+
+
+    def downloadMovie(self, title='', video_url=''):
+        """Downlod YouTube video into folder
+
+        Args:
+            Requires - folder (str) - The video title to store the downloaded video
+            Requires - video_url (str) - The link of the YouTube video
+        """
+        self.folder = os.path.join(self.path, title)
+        self.f_name = os.path.join(self.path, title, f'{title}.mp4')
+        if not os.path.exists(self.folder):
+            print(f'creating directory: "{self.folder}"')
+            os.mkdir(self.folder)
+
+        ytdl_opts = {
+            'writesubtitles': True,
+            'subtitle': '--write-sub --sub-lang en',
+            'cookiefile': self.cookies,
+            'format': "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best",
+            'outtmpl': self.f_name,
+            'postprocessors': [{
+                'key': 'FFmpegEmbedSubtitle'
+            }],
+            'logger': MyLogger(),
+            'progress_hooks': [my_hook]
+        }
+        with youtube_dl.YoutubeDL(ytdl_opts) as ytdl:
+            ytdl.download([video_url])
 
 
     def getInfo(self, path='', video_url=''):

f18f57494f5205b9b6d731139b64b563ee8f5b10
kkatayama <katayama@udel.edu>
2021-04-14 11:30:39 -0400

    extend SonarrAPI ...

--- plexarr/sonarr_api.py
+++ plexarr/sonarr_api.py
@@ -107,10 +107,10 @@ class SonarrAPI(RequestsAPI):
         if ((s_num >= 0) and (e_num >= 0) and (title)):
             ep_all = self.getEpisodes(title=title)
             ep_info = next(filter(lambda x: x['seasonNumber'] == s_num and x['episodeNumber'] == e_num, ep_all), None)
-            episode_id = ep_info["episodeFileId"]
+            episode_file_id = ep_info["episodeFileId"]
             c.print(ep_all)
             c.print(ep_info)
-            c.print(episode_id)
+            c.print(episode_file_id)
 
         path = f'/EpisodeFile/{episode_file_id}'
         res = self.get(path=path)

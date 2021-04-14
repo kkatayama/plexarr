140f1286d5e86530f9b51cb10f0366f36fd5a438
kkatayama <katayama@udel.edu>
2021-04-13 20:46:01 -0400

    added SonarrAPI

--- plexarr/__init__.py
+++ plexarr/__init__.py
@@ -1,7 +1,8 @@
 from .radarr_api import RadarrAPI
+from .sonarr_api import SonarrAPI
 from .youtube_api import YouTubeAPI
 from .tmdb_api import TmdbAPI
 from .ombi_api import OmbiAPI
 from .plex_api import PlexAPI
 
-__all__ = ['RadarrAPI', 'TmdbAPI', 'YouTubeAPI', 'OmbiAPI', 'PlexAPI']
+__all__ = ['RadarrAPI', 'SonarrAPI', 'TmdbAPI', 'YouTubeAPI', 'OmbiAPI', 'PlexAPI']

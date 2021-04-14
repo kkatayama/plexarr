15ad6b7f08f5a132fba7574ad4a42cf39b33c82e
kkatayama <katayama@udel.edu>
2021-04-14 12:25:52 -0400

    extend SonarrAPI ...

--- plexarr/htpc_api.py
+++ plexarr/htpc_api.py
@@ -42,3 +42,19 @@ class HTPC_API(object):
                 scp.put(files=folder, remote_path=host['movies'], recursive=True)
         return os.path.join(host['movies'], os.path.split(folder)[1])
 
+    def uploadSeries(self, folder):
+        """Upload movie directory containing movie file to host["mal"]
+
+        Args:
+            Requires - folder (str)  - The local path of the downloaded movie
+        Returns:
+            movie_path (str) - The remote path of the uploaded movie (folder)
+        """
+        host = dict(self.mal.items())
+        with SSHClient() as ssh:
+            ssh.load_system_host_keys()
+            ssh.connect(hostname=host['ip'], port=host['port'], username=host['username'])
+            with SCPClient(ssh.get_transport(), progress4=progress4) as scp:
+                scp.put(files=folder, remote_path=host['series'], recursive=True)
+        return os.path.join(host['series'], os.path.split(folder)[1])
+
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='plexarr',
-    version='0.4.6',
+    version='0.4.7',
     author='Teddy Katayama',
     author_email='katayama@udel.edu',
     description='Unofficial Python Wrapper for the Plex, Sonarr, Radarr, and Bazarr API with Added Features',

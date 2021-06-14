84d57bec9f5a4e08e6fa12777122a19bfe89ab85
kkatayama <katayama@udel.edu>
2021-06-14 12:46:06 -0400

    fix _base_path

--- plexarr/__init__.py
+++ plexarr/__init__.py
@@ -7,4 +7,4 @@ from .plex_api import PlexAPI
 from .nzbhydra_api import NZBHydraAPI
 
 __all__ = ['RadarrAPI', 'SonarrAPI', 'TmdbAPI', 'YouTubeAPI', 'OmbiAPI', 'PlexAPI', 'NZBHydraAPI']
-__version__ = "0.8.9"
+__version__ = "0.9.0"

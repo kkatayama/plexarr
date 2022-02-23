db29591865888786160ca6175c0e558061216813
kkatayama <katayama@udel.edu>
2022-02-23 05:59:40 -0500

    updating: YoutubeDLP()

--- plexarr/yt_dlp_api.py
+++ plexarr/yt_dlp_api.py
@@ -148,7 +148,7 @@ class YouTubeDLP(object):
             'noplaylist': True,
             'ignoreerrors': True,
             'cookiefile': self.cookies,
-            'default_search': 'ytsearch5',
+            'default_search': 'ytsearch10',
             'skip_download': True,
             'format': f'{video_format}+{audio_format}',
             'logger': MyLogger(),

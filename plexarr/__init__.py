770723b56fa5fff4ddf82bc62469d401da579d94
kkatayama <katayama@udel.edu>
2022-03-02 15:59:59 -0500

    added: python-plexapi

--- plexarr/__init__.py
+++ plexarr/__init__.py
@@ -4,12 +4,26 @@ from .kemo_api import KemoAPI
 from .mount_api import MountAPI
 from .nzbhydra_api import NZBHydraAPI
 from .ombi_api import OmbiAPI
-from .plex_api import PlexAPI
+from .plex_api import PlexAPI, PlexPy
 from .radarr_api import RadarrAPI
 from .sonarr_api import SonarrAPI
 from .tmdb_api import TmdbAPI
 from .youtube_api import YouTubeAPI
 from .yt_dlp_api import YouTubeDLP
 
-__all__ = ['RadarrAPI', 'SonarrAPI', 'TmdbAPI', 'YouTubeAPI', 'YouTubeDLP', 'OmbiAPI', 'PlexAPI', 'NZBHydraAPI', 'GitHubAPI', 'KemoAPI', 'ChapoAPI', 'MountAPI']
+__all__ = [
+    'RadarrAPI',
+    'SonarrAPI',
+    'TmdbAPI',
+    'YouTubeAPI',
+    'YouTubeDLP',
+    'OmbiAPI',
+    'PlexAPI',
+    'PlexPy',
+    'NZBHydraAPI',
+    'GitHubAPI',
+    'KemoAPI',
+    'ChapoAPI',
+    'MountAPI'
+]
 __version__ = "1.1.106"

8bc3e478eaa8d6e2d5bf4c37a7cba74239e9dd6e
kkatayama <kkatayama@udel.edu>
2022-03-11 12:10:08 -0500

    ssh cmd

--- plexarr/htpc_api.py
+++ plexarr/htpc_api.py
@@ -196,6 +196,6 @@ class HTPC_API(object):
         host = dict(self.imac.items())
         # path = f'/Users/{host["username"]}/bin/{cmd}'
         path = f'{cmd}'
-        ssh_cmd = f'ssh -t -p {host["port"]} {host["username"]}@{host["ip"]} "{path}"'
+        ssh_cmd = f'ssh -t -p {host["port"]} {host["username"]}@{host["ip"]} \'{path}\''
         output = subprocess.run(ssh_cmd, shell=True, capture_output=True, text=True).stdout.strip()
         return markdown.markdown(''.join([f"    {l}\n" for l in output.splitlines()]))

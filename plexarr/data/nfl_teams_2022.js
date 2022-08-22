b3e4374bd7d0b8bb3b04caf6b62f86e5ec3f3a76
kkatayama <kkatayama@udel.edu>
2022-08-22 06:34:51 -0400

    espn_api: getTeams()

---
+++ plexarr/data/nfl_teams_2022.js
@@ -0,0 +1,258 @@
+[
+  {
+    "team_id": "22",
+    "team_name": "Arizona Cardinals",
+    "team_nick": "Cardinals",
+    "team_abbr": "ARI",
+    "team_area": "Arizona",
+    "team_venue": "State Farm Stadium"
+  },
+  {
+    "team_id": "1",
+    "team_name": "Atlanta Falcons",
+    "team_nick": "Falcons",
+    "team_abbr": "ATL",
+    "team_area": "Atlanta",
+    "team_venue": "Mercedes-Benz Stadium"
+  },
+  {
+    "team_id": "33",
+    "team_name": "Baltimore Ravens",
+    "team_nick": "Ravens",
+    "team_abbr": "BAL",
+    "team_area": "Baltimore",
+    "team_venue": "M&T Bank Stadium"
+  },
+  {
+    "team_id": "2",
+    "team_name": "Buffalo Bills",
+    "team_nick": "Bills",
+    "team_abbr": "BUF",
+    "team_area": "Buffalo",
+    "team_venue": "Highmark Stadium"
+  },
+  {
+    "team_id": "29",
+    "team_name": "Carolina Panthers",
+    "team_nick": "Panthers",
+    "team_abbr": "CAR",
+    "team_area": "Carolina",
+    "team_venue": "Bank of America Stadium"
+  },
+  {
+    "team_id": "3",
+    "team_name": "Chicago Bears",
+    "team_nick": "Bears",
+    "team_abbr": "CHI",
+    "team_area": "Chicago",
+    "team_venue": "Soldier Field"
+  },
+  {
+    "team_id": "4",
+    "team_name": "Cincinnati Bengals",
+    "team_nick": "Bengals",
+    "team_abbr": "CIN",
+    "team_area": "Cincinnati",
+    "team_venue": "Paycor Stadium"
+  },
+  {
+    "team_id": "5",
+    "team_name": "Cleveland Browns",
+    "team_nick": "Browns",
+    "team_abbr": "CLE",
+    "team_area": "Cleveland",
+    "team_venue": "FirstEnergy Stadium"
+  },
+  {
+    "team_id": "6",
+    "team_name": "Dallas Cowboys",
+    "team_nick": "Cowboys",
+    "team_abbr": "DAL",
+    "team_area": "Dallas",
+    "team_venue": "AT&T Stadium"
+  },
+  {
+    "team_id": "7",
+    "team_name": "Denver Broncos",
+    "team_nick": "Broncos",
+    "team_abbr": "DEN",
+    "team_area": "Denver",
+    "team_venue": "Empower Field at Mile High"
+  },
+  {
+    "team_id": "8",
+    "team_name": "Detroit Lions",
+    "team_nick": "Lions",
+    "team_abbr": "DET",
+    "team_area": "Detroit",
+    "team_venue": "Ford Field"
+  },
+  {
+    "team_id": "9",
+    "team_name": "Green Bay Packers",
+    "team_nick": "Packers",
+    "team_abbr": "GB",
+    "team_area": "Green Bay",
+    "team_venue": "Lambeau Field"
+  },
+  {
+    "team_id": "34",
+    "team_name": "Houston Texans",
+    "team_nick": "Texans",
+    "team_abbr": "HOU",
+    "team_area": "Houston",
+    "team_venue": "NRG Stadium"
+  },
+  {
+    "team_id": "11",
+    "team_name": "Indianapolis Colts",
+    "team_nick": "Colts",
+    "team_abbr": "IND",
+    "team_area": "Indianapolis",
+    "team_venue": "Lucas Oil Stadium"
+  },
+  {
+    "team_id": "30",
+    "team_name": "Jacksonville Jaguars",
+    "team_nick": "Jaguars",
+    "team_abbr": "JAX",
+    "team_area": "Jacksonville",
+    "team_venue": "TIAA Bank Field"
+  },
+  {
+    "team_id": "12",
+    "team_name": "Kansas City Chiefs",
+    "team_nick": "Chiefs",
+    "team_abbr": "KC",
+    "team_area": "Kansas City",
+    "team_venue": "GEHA Field at Arrowhead Stadium"
+  },
+  {
+    "team_id": "13",
+    "team_name": "Las Vegas Raiders",
+    "team_nick": "Raiders",
+    "team_abbr": "LV",
+    "team_area": "Las Vegas",
+    "team_venue": "Allegiant Stadium"
+  },
+  {
+    "team_id": "24",
+    "team_name": "Los Angeles Chargers",
+    "team_nick": "Chargers",
+    "team_abbr": "LAC",
+    "team_area": "Los Angeles",
+    "team_venue": "Dignity Health Sports Park"
+  },
+  {
+    "team_id": "14",
+    "team_name": "Los Angeles Rams",
+    "team_nick": "Rams",
+    "team_abbr": "LAR",
+    "team_area": "Los Angeles",
+    "team_venue": "Los Angeles Memorial Coliseum"
+  },
+  {
+    "team_id": "15",
+    "team_name": "Miami Dolphins",
+    "team_nick": "Dolphins",
+    "team_abbr": "MIA",
+    "team_area": "Miami",
+    "team_venue": "Hard Rock Stadium"
+  },
+  {
+    "team_id": "16",
+    "team_name": "Minnesota Vikings",
+    "team_nick": "Vikings",
+    "team_abbr": "MIN",
+    "team_area": "Minnesota",
+    "team_venue": "U.S. Bank Stadium"
+  },
+  {
+    "team_id": "17",
+    "team_name": "New England Patriots",
+    "team_nick": "Patriots",
+    "team_abbr": "NE",
+    "team_area": "New England",
+    "team_venue": "Gillette Stadium"
+  },
+  {
+    "team_id": "18",
+    "team_name": "New Orleans Saints",
+    "team_nick": "Saints",
+    "team_abbr": "NO",
+    "team_area": "New Orleans",
+    "team_venue": "Caesars Superdome"
+  },
+  {
+    "team_id": "19",
+    "team_name": "New York Giants",
+    "team_nick": "Giants",
+    "team_abbr": "NYG",
+    "team_area": "New York",
+    "team_venue": "MetLife Stadium"
+  },
+  {
+    "team_id": "20",
+    "team_name": "New York Jets",
+    "team_nick": "Jets",
+    "team_abbr": "NYJ",
+    "team_area": "New York",
+    "team_venue": "MetLife Stadium"
+  },
+  {
+    "team_id": "21",
+    "team_name": "Philadelphia Eagles",
+    "team_nick": "Eagles",
+    "team_abbr": "PHI",
+    "team_area": "Philadelphia",
+    "team_venue": "Lincoln Financial Field"
+  },
+  {
+    "team_id": "23",
+    "team_name": "Pittsburgh Steelers",
+    "team_nick": "Steelers",
+    "team_abbr": "PIT",
+    "team_area": "Pittsburgh",
+    "team_venue": "Acrisure Stadium"
+  },
+  {
+    "team_id": "25",
+    "team_name": "San Francisco 49ers",
+    "team_nick": "49ers",
+    "team_abbr": "SF",
+    "team_area": "San Francisco",
+    "team_venue": "Levi's Stadium"
+  },
+  {
+    "team_id": "26",
+    "team_name": "Seattle Seahawks",
+    "team_nick": "Seahawks",
+    "team_abbr": "SEA",
+    "team_area": "Seattle",
+    "team_venue": "Lumen Field"
+  },
+  {
+    "team_id": "27",
+    "team_name": "Tampa Bay Buccaneers",
+    "team_nick": "Buccaneers",
+    "team_abbr": "TB",
+    "team_area": "Tampa Bay",
+    "team_venue": "Raymond James Stadium"
+  },
+  {
+    "team_id": "10",
+    "team_name": "Tennessee Titans",
+    "team_nick": "Titans",
+    "team_abbr": "TEN",
+    "team_area": "Tennessee",
+    "team_venue": "Nissan Stadium"
+  },
+  {
+    "team_id": "28",
+    "team_name": "Washington Commanders",
+    "team_nick": "Commanders",
+    "team_abbr": "WSH",
+    "team_area": "Washington",
+    "team_venue": "FedExField"
+  }
+]

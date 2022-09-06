67f979a4e3a88baa006eaafd99a16b83e231ca1c
kkatayama <kkatayama@udel.edu>
2022-09-06 10:56:01 -0400

    utils: get_py_path()

--- plexarr/utils.py
+++ plexarr/utils.py
@@ -18,8 +18,8 @@ from contextlib import contextmanager
 
 def get_py_path():
     # return Path(globals()['_dh'][0]) if globals().get('_dh') else Path(__file__)
-    env_info = inspect.currentframe().f_back.f_locals
-    return Path(env_info["_dh"][0]) if env_info.get('_dh') else Path(env_info["__file__"])
+    env = inspect.currentframe().f_back.f_locals
+    return Path(env["_dh"][0]) if env.get('_dh') else Path(env["__file__"]).parent
 
 # -- https://stackoverflow.com/a/41510011/3370913
 def camel_case(s):

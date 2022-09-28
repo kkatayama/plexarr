e2ae38012efb07ed84a45e5268f20491ebcd9c29
kkatayama <kkatayama@udel.edu>
2022-09-28 11:53:48 -0400

    kemo_api: iptv=lemo, lemo2

--- plexarr/kemo_api.py
+++ plexarr/kemo_api.py
@@ -40,27 +40,39 @@ class KemoAPI(object):
         config = ConfigParser()
         config.read(Path(Path.home(), '.config/plexarr.ini'))
 
-        self.API_URL = config["kemo"].get('api_url')
-        self.USERNAME = config["kemo"].get('username')
-        self.PASSWORD = config["kemo"].get('password')
-        self.PARAMS = {
-            "username": self.USERNAME,
-            "password": self.PASSWORD
+        self.lemo = {
+            "api_url": config["lemo"].get("api_url"),
+            "username": config["lemo"].get("username"),
+            "password": config["lemo"].get("password"),
+            "params": {
+                "username": config["lemo"].get("username"),
+                "password": config["lemo"].get("password")
+            },
+        }
+        self.lemo2 = {
+            "api_url": config["lemo2"].get("api_url"),
+            "username": config["lemo2"].get("username"),
+            "password": config["lemo2"].get("password"),
+            "params": {
+                "username": config["lemo2"].get("username"),
+                "password": config["lemo2"].get("password")
+            },
         }
         self.CATEGORY = {}
         self.STREAMS = {}
         self.espn = ESPN_API()
 
-    def getCategories(self, groups='', terms=''):
+    def getCategories(self, groups='', terms='', iptv='lemo'):
         """
         Get All Categories in Matching Groups
 
         ARGS:
             groups (str|list) - ex: "USA Sports" or ['USA News', 'USA Sports']
         """
-        payload = self.PARAMS
+        API_URL = eval(f'self.{iptv}["api_url"]')
+        payload = eval(f'self.{iptv}["params"]')
         payload.update({'action': 'get_live_categories'})
-        r = requests.get(url=self.API_URL, params=payload)
+        r = requests.get(url=API_URL, params=payload)
 
         if groups:
             return list(filter(lambda x: x["category_name"] in groups, r.json()))
@@ -69,26 +81,29 @@ class KemoAPI(object):
             return list(filter(lambda x: any(term in x["category_name"].lower() for term in terms), r.json()))
         return r.json()
 
-    def getCategory(self, query=''):
+    def getCategory(self, query='', iptv='lemo'):
         """Get Category using query filter"""
-        payload = self.PARAMS
+        API_URL = eval(f'self.{iptv}["api_url"]')
+        payload = eval(f'self.{iptv}["params"]')
         payload.update({'action': 'get_live_categories'})
-        r = requests.get(url=self.API_URL, params=payload)
+        r = requests.get(url=API_URL, params=payload)
         return next((cat for cat in r.json() if query.lower() in cat.get('category_name').lower()), {})
 
-    def setCategory(self, query=''):
+    def setCategory(self, query='', iptv='lemo'):
         """Set Category using 'query' filter"""
-        payload = self.PARAMS
+        API_URL = eval(f'self.{iptv}["api_url"]')
+        payload = eval(f'self.{iptv}["params"]')
         payload.update({'action': 'get_live_categories'})
-        r = requests.get(url=self.API_URL, params=payload)
+        r = requests.get(url=API_URL, params=payload)
         self.CATEGORY = next((cat for cat in r.json() if query.lower() in cat.get('category_name').lower()), {})
 
-    def getStreams(self, terms=''):
+    def getStreams(self, terms='', iptv='lemo'):
         """Get Streams by Category_ID and filtered by 'terms'"""
-        payload = self.PARAMS
+        API_URL = eval(f'self.{iptv}["api_url"]')
+        payload = eval(f'self.{iptv}["params"]')
         payload.update({'action': 'get_live_streams'})
         payload.update({'category_id': self.CATEGORY.get("category_id")})
-        r = requests.get(url=self.API_URL, params=payload)
+        r = requests.get(url=API_URL, params=payload)
         streams = r.json()
 
         if isinstance(terms, str):
@@ -100,77 +115,87 @@ class KemoAPI(object):
             return all_streams
         return streams
 
-    def getStreamsNFL(self):
+    def getStreamsNFL(self, iptv='lemo'):
         """Get NFL Streams"""
         self.setCategory(query="NFL")
-        streams = self.getStreams(terms=["USA NFL Thursday Night", "USA NFL Sunday Night", "USA NFL Monday Night", "USA NFL Sunday 7"])
+        terms = ["USA NFL Thursday Night", "USA NFL Sunday Night", "USA NFL Monday Night", "USA NFL Sunday 7"]
+        streams = self.getStreams(terms=terms, iptv=iptv)
         return streams
 
-    def getStreamsNBA(self):
+    def getStreamsNBA(self, iptv='lemo'):
         """Get NBA Streams"""
         self.setCategory(query="NBA")
-        streams_1 = self.getStreams(terms="USA NBA 0")
-        streams_2 = self.getStreams(terms="USA NBA 1")
+        streams_1 = self.getStreams(terms="USA NBA 0", iptv=iptv)
+        streams_2 = self.getStreams(terms="USA NBA 1", iptv=iptv)
         streams = chain(streams_1, streams_2)
         return streams
 
-    def getStreamsESPN(self, terms=""):
+    def getStreamsESPN(self, terms="", iptv='lemo'):
         """GET ESPN PLUS STREAMS"""
         self.setCategory(query="ESPN")
-        streams = self.getStreams(terms=terms)
+        streams = self.getStreams(terms=terms, iptv=iptv)
         return streams
 
-
-    def m3uNFL(self):
+    def m3uNFL(self, iptv='lemo'):
         """Generate m3u for NFL Streams"""
+        API_URL = eval(f'self.{iptv}["api_url"]')
+        USERNAME = eval(f'self.{iptv}["username"]')
+        PASSWORD = eval(f'self.{iptv}["password"]')
         m3u = "#EXTM3U\n"
         tvg_cuid = 702
-        for i, stream in enumerate(self.getStreamsNFL()):
+        for i, stream in enumerate(self.getStreamsNFL(iptv=iptv)):
             tvg_id = stream.get("stream_id")
             tvg_name = stream.get("name").split(":")[0].strip()
             tvg_logo = "http://line.lemotv.cc/images/d7a1c666d3827922b7dfb5fbb9a3b450.png"
             tvg_group = "NFL Sunday Games"
 
             m3u += f'#EXTINF:-1 CUID="{tvg_cuid}" tvg-id="{tvg_id}" tvg-name="{tvg_name}" tvg-logo="{tvg_logo}" group-title="{tvg_group}",{tvg_name}\n'
-            m3u += self.API_URL.replace('/player_api.php', f'/{self.USERNAME}/{self.PASSWORD}/{tvg_id}\n')
+            m3u += API_URL.replace('/player_api.php', f'/{USERNAME}/{PASSWORD}/{tvg_id}\n')
             tvg_cuid += 1
         return m3u
 
-    def m3uNBA(self):
+    def m3uNBA(self, iptv='lemo'):
         """Generate m3u for NBA Streams"""
+        API_URL = eval(f'self.{iptv}["api_url"]')
+        USERNAME = eval(f'self.{iptv}["username"]')
+        PASSWORD = eval(f'self.{iptv}["password"]')
         m3u = "#EXTM3U\n"
         tvg_cuid = 100
-        for i, stream in enumerate(self.getStreamsNBA()):
+        for i, stream in enumerate(self.getStreamsNBA(iptv=iptv)):
             tvg_id = stream.get("stream_id")
             tvg_name = stream.get("name").split(":")[0].strip()
             tvg_logo = "http://line.lemotv.cc/images/118ae626674246e6d081a4ff16921b19.png"
             tvg_group = "NBA Games"
 
             m3u += f'#EXTINF:-1 CUID="{tvg_cuid}" tvg-id="{tvg_id}" tvg-name="{tvg_name}" tvg-logo="{tvg_logo}" group-title="{tvg_group}",{tvg_name}\n'
-            m3u += self.API_URL.replace('/player_api.php', f'/{self.USERNAME}/{self.PASSWORD}/{tvg_id}\n')
+            m3u += API_URL.replace('/player_api.php', f'/{USERNAME}/{PASSWORD}/{tvg_id}\n')
             tvg_cuid += 1
         return m3u
 
-    def m3uESPN(self, terms=""):
+    def m3uESPN(self, terms="", iptv='lemo'):
         """Generate m3u for ESPN PLUS Streams"""
+        API_URL = eval(f'self.{iptv}["api_url"]')
+        USERNAME = eval(f'self.{iptv}["username"]')
+        PASSWORD = eval(f'self.{iptv}["password"]')
         m3u = "#EXTM3U\n"
         tvg_cuid = 1500
-        for i, stream in enumerate(self.getStreamsESPN(terms=terms)):
+        for i, stream in enumerate(self.getStreamsESPN(terms=terms, iptv=iptv)):
             tvg_id = stream.get("stream_id")
             tvg_name = stream.get("name").split(":")[0].strip()
             tvg_logo = "https://artwork.espncdn.com/programs/14ef54cc-6fd8-443d-80b8-365c1f64d606/16x9/large_20211213222642.jpg"
             tvg_group = "ESPN+"
 
             m3u += f'#EXTINF:-1 CUID="{tvg_cuid}" tvg-id="{tvg_id}" tvg-name="{tvg_name}" tvg-logo="{tvg_logo}" group-title="{tvg_group}",{tvg_name}\n'
-            m3u += self.API_URL.replace('/player_api.php', f'/{self.USERNAME}/{self.PASSWORD}/{tvg_id}\n')
+            m3u += API_URL.replace('/player_api.php', f'/{USERNAME}/{PASSWORD}/{tvg_id}\n')
             tvg_cuid += 1
         return m3u
 
-    def xmlNFL(self):
+    def xmlNFL(self, iptv='lemo'):
         """Generate xml for NFL Streams"""
+        API_URL = eval(f'self.{iptv}["api_url"]')
         channels = []
         programs = []
-        for stream in self.getStreamsNFL():
+        for stream in self.getStreamsNFL(iptv=iptv):
             tvg_id = stream.get("stream_id")
             tvg_name = stream.get("name").split(":")[0].strip()
             tvg_logo = "http://line.lemotv.cc/images/d7a1c666d3827922b7dfb5fbb9a3b450.png"
@@ -197,15 +222,16 @@ class KemoAPI(object):
                     pass
 
         # return gen_xmltv_xml(channels=channels, programs=programs, url=self.API_URL)
-        url = furl(self.API_URL).origin
+        url = furl(API_URL).origin
         tpl = str(Path(__file__).parent.joinpath("templates/epg.tpl"))
         return template(tpl, channels=channels, programs=programs, url=url)
 
-    def xmlNBA(self):
+    def xmlNBA(self, iptv='lemo'):
         """Generate xml NBA Streams"""
+        API_URL = eval(f'self.{iptv}["api_url"]')
         channels = []
         programs = []
-        for stream in self.getStreamsNBA():
+        for stream in self.getStreamsNBA(iptv=iptv):
             tvg_id = stream.get("stream_id")
             tvg_name = stream.get("name").split(":")[0].strip()
             tvg_logo = "http://line.lemotv.cc/images/118ae626674246e6d081a4ff16921b19.png"
@@ -229,12 +255,13 @@ class KemoAPI(object):
                 except Exception:
                     pass
         # return gen_xmltv_xml(channels=channels, programs=programs, url=self.API_URL)
-        url = furl(self.API_URL).origin
+        url = furl(API_URL).origin
         tpl = str(Path(__file__).parent.joinpath("templates/epg.tpl"))
         return template(tpl, channels=channels, programs=programs, url=url)
 
-    def xmlESPN(self, terms=""):
+    def xmlESPN(self, terms="", iptv='lemo'):
         """Generate xml NBA Streams"""
+        API_URL = eval(f'self.{iptv}["api_url"]')
         channels = []
         programs = []
         for stream in self.getStreamsESPN(terms=terms):
@@ -260,6 +287,6 @@ class KemoAPI(object):
                 except Exception:
                     pass
         # return gen_xmltv_xml(channels=channels, programs=programs, url=self.API_URL)
-        url = furl(self.API_URL).origin
+        url = furl(API_URL).origin
         tpl = str(Path(__file__).parent.joinpath("templates/epg.tpl"))
         return template(tpl, channels=channels, programs=programs, url=url)

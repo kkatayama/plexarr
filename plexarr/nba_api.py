353f131de78dd48e0e5b5f95387927f15f312d28
kkatayama <kkatayama@udel.edu>
2022-10-12 00:43:13 -0400

    nba_api: cache nba_schedule_YEAR.csv

--- plexarr/nba_api.py
+++ plexarr/nba_api.py
@@ -69,25 +69,30 @@ class NBA_API(object):
                 })
         return pd.concat([df_teams, pd.DataFrame.from_records(teams)])
 
-    def getNBASchedule(self, year=0):
+    def getNBASchedule(self, year=0, update=False):
         df_teams = self.df_teams
         year = year if year else self.YEAR
         path_schedule = f'/prod/v2/{year}/schedule.json'
-        schedule = []
-        for event in self.get(path=path_schedule)["league"]["standard"]:
-            game_time = pd.to_datetime(event["startTimeUTC"]).tz_convert(tz='US/Eastern')
-            home = df_teams[df_teams["team_id"] == event["hTeam"]["teamId"]].squeeze()
-            away = df_teams[df_teams["team_id"] == event["vTeam"]["teamId"]].squeeze()
-            game = {
-                "day_start": game_time.replace(hour=0, minute=0, second=0),
-                "day_end": game_time.replace(hour=23, minute=59, second=59),
-                "game_time": game_time,
-                "home_team": "" if home.empty else home["team_name"] ,
-                "home_venue": "" if home.empty else home["team_venue"],
-                "away_team": "" if away.empty else away["team_name"],
-            }
-            schedule.append(game)
-        return pd.DataFrame.from_records(schedule)
+        csv = Path(__file__).parent.joinpath(f'data/nba_schedule_{year}.csv')
+        if csv.exists() and not update:
+            df_schedule = read_csv(csv)
+        else:
+            schedule = []
+            for event in self.get(path=path_schedule)["league"]["standard"]:
+                game_time = pd.to_datetime(event["startTimeUTC"]).tz_convert(tz='US/Eastern')
+                home = df_teams[df_teams["team_id"] == event["hTeam"]["teamId"]].squeeze()
+                away = df_teams[df_teams["team_id"] == event["vTeam"]["teamId"]].squeeze()
+                game = {
+                    "day_start": game_time.replace(hour=0, minute=0, second=0),
+                    "day_end": game_time.replace(hour=23, minute=59, second=59),
+                    "game_time": game_time,
+                    "home_team": "" if home.empty else home["team_name"] ,
+                    "home_venue": "" if home.empty else home["team_venue"],
+                    "away_team": "" if away.empty else away["team_name"],
+                }
+                schedule.append(game)
+            df_schedule = pd.DataFrame.from_records(schedule)
+        return df_schedule
 
     def parseNBAInfo(self, line):
         """
@@ -95,6 +100,7 @@ class NBA_API(object):
         from rich import print
         import re
 
+        nba = NBA_API()
         tests = [
             "USA NBA 01: Miami Heat vs Boston Celtics @ 08:30 PM",
             "USA NBA 02: Miami Heat vs Boston Celtics @ 08:30 PM",

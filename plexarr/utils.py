4b1c2678991e1727987dfdd52e08f0b67eda921b
kkatayama <kkatayama@udel.edu>
2022-11-23 04:40:21 -0500

    utils: get_py_path() - support nested...

--- plexarr/utils.py
+++ plexarr/utils.py
@@ -31,12 +31,16 @@ import requests
 
 def get_py_path(verbose=False):
     # return Path(globals()['_dh'][0]) if globals().get('_dh') else Path(__file__)
+    print('starting at currentframe().f_back') if verbose else ''
     env = inspect.currentframe().f_back.f_locals
     if ((not env.get('_dh')) and (not env.get('__file__'))):
+        print('going deeper: currentframe().f_back.f_back') if verbose else ''
         env = inspect.currentframe().f_back.f_back.f_locals
+        if ((not env.get('_dh')) and (not env.get('__file__'))):
+            print('even deeper: currentframe().f_back.f_back.f_back') if verbose else ''
+            env = inspect.currentframe().f_back.f_back.f_back.f_locals
     if env.get('_dh'):
-        if verbose:
-            print('==ipython shell==')
+        print('==ipython shell==') if verbose else ''
         if env.get('__file__'):
             return Path(env["_dh"][0], env["__file__"]).resolve().parent
 
@@ -45,7 +49,10 @@ def get_py_path(verbose=False):
             print('Next time run with:\n  ipython -i -- <File.py>')
             print('using cwd()')
         return Path(env["_dh"][0])
+
+    print(f'env = {env}') if verbose else ''
     return Path(env["__file__"]).resolve().parent
+
 
 # -- https://stackoverflow.com/a/41510011/3370913
 def camel_case(s):
